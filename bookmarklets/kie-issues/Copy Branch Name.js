aa363bd202c406f6688f2c42b82f37ae4349e4ca
fantonangeli <fabrizio.antonangeli@gmail.com>
2024-03-07 15:17:28 +0100

    add bookmarklets/kie-issues/Copy Branch Name.js

---
+++ bookmarklets/kie-issues/Copy Branch Name.js
@@ -0,0 +1,1 @@
+javascript: var branchName = document.title.replace(/^^([^·]+) · Issue #(\d+) .*$/g, "kie-issues#$2: $1").replace(/\s{1,}/mg, "-").replace(/--*/mg, "-").replace(/\[(.+)\]/, "$1").replace(/[^a-zA-Z0-9-#]+/g, ""); navigator.clipboard.writeText(branchName); alert("Copied the text: " + branchName);
