0ae8758083b6236e3182983875d4849dc476b7e1
fantonangeli <fabrizio.antonangeli@gmail.com>
2024-10-14 17:46:16 +0200

    Fixed text

--- bookmarklets/kie-issues/Copy PR Description.js
+++ bookmarklets/kie-issues/Copy PR Description.js
@@ -1,1 +1,1 @@
-javascript: var link = window.location.href; var description = document.querySelector(".comment-body").innerText; var prDesc = `**Closes:** ${link}\n\n**Description:**\n${description}\n\n**Preview:**\n`; navigator.clipboard.writeText(prDesc); alert("Copied the text:\n " + prDesc)
+javascript: var link = window.location.href; var description = document.querySelector(".comment-body").innerText; var prDesc = `**Closes** ${link}\n\n**Description:**\n${description}\n\n**Preview:**\n`; navigator.clipboard.writeText(prDesc); alert("Copied the text:\n " + prDesc)
