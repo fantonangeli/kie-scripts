f4381a6a61148aea2ddd7eeb6c0295be731b921f
fantonangeli <fabrizio.antonangeli@gmail.com>
2025-01-17 17:40:55 +0100

    Change GH closing keyword

--- bookmarklets/kie-issues/Copy PR Description.js
+++ bookmarklets/kie-issues/Copy PR Description.js
@@ -1,6 +1,6 @@
 javascript: 
 var link = window.location.href; 
-var description = document.querySelector(".comment-body").innerText; 
-var prDesc = `**Closes** ${link}\n\n**Description:**\n${description}\n\n**Preview:**\n`; 
+var description = document.querySelector('.markdown-body').innerText ; 
+var prDesc = `Closes ${link}\n\n**Description:**\n${description}\n\n**Preview:**\n`; 
 navigator.clipboard.writeText(prDesc); 
 alert("Copied the text:\n " + prDesc)
