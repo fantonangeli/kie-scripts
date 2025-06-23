66d01bd91ad59c2adc011d115aa5c7b1fbc5641e
fantonangeli <fabrizio.antonangeli@gmail.com>
2025-06-23 12:17:26 +0200

    Rename kie-tools-issues to kie-tools when referring to issues

--- bookmarklets/kie-issues/Copy Branch Name.js
+++ bookmarklets/kie-issues/Copy Branch Name.js
@@ -1,6 +1,6 @@
 javascript:
 var issuePrefix="kie-issues#";
-window.location.href.startsWith("https://github.com/apache/incubator-kie-tools") && (issuePrefix="kie-tools-issues#");
+window.location.href.startsWith("https://github.com/apache/incubator-kie-tools") && (issuePrefix="kie-tools#");
 var branchName = document.title.replace(/^^([^·]+) · Issue #(\d+) .*$/g, `${issuePrefix}$2: $1`).replace(/\s{1,}/mg, "-").replace(/--*/mg, "-").replace(/\[(.+)\]/, "$1").replace(/[^a-zA-Z0-9-#]+/g, ""); 
 navigator.clipboard.writeText(branchName); 
 alert("Copied the text: " + branchName);
--- bookmarklets/kie-issues/Copy PR title.js
+++ bookmarklets/kie-issues/Copy PR title.js
@@ -1,6 +1,6 @@
 javascript: 
 var issuePrefix="kie-issues#";
-window.location.href.startsWith("https://github.com/apache/incubator-kie-tools") && (issuePrefix="kie-tools-issues#");
+window.location.href.startsWith("https://github.com/apache/incubator-kie-tools") && (issuePrefix="kie-tools#");
 var text = document.title.replace(/^^([^·]+) · Issue #(\d+) .*$/g, `${issuePrefix}$2: $1`); 
 navigator.clipboard.writeText(text); 
 alert("Copied the text: " + text);
