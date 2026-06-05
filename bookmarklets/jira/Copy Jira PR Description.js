e89d8c49bea896b8cb09298279e362ae5128e4b9
fantonangeli <fabrizio.antonangeli@gmail.com>
2026-06-05 16:59:41 +0200

    Updated jira PR description selector

--- bookmarklets/jira/Copy Jira PR Description.js
+++ bookmarklets/jira/Copy Jira PR Description.js
@@ -1,5 +1,5 @@
 javascript: 
 var link = window.location.href; 
-var description = document.querySelector("#description-val").innerText; 
+var description = document.querySelector('[data-testid="issue.views.field.rich-text.description"]').innerText; 
 var prDesc = `**Jira:** ${link}\n\n**Description:**\n${description}\n\n**Preview:**\n`; 
 navigator.clipboard.writeText(prDesc); alert("Copied the text: " + prDesc);
